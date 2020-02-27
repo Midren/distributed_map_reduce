e392f275ed2da0348ac25f02627fa84b33605bc7
Roman Milishchuk <milromchuk@gmail.com>
2020-02-27 18:20:41 +0200

    Minor changes

--- main.cpp
+++ main.cpp
@@ -7,15 +7,17 @@
 #include "JobConf.h"
 
 int main() {
-    auto *library_handler = dlopen("libmap_reduce_configurator_library.so", RTLD_LAZY);
+    auto library_handler = dlopen("libmap_reduce_configurator.so", RTLD_LAZY);
+
     if (library_handler == nullptr) {
         std::cout << "Cannot open shared library: " << dlerror() << std::endl;
     }
+
     auto get_cfg = (get_config_t) dlsym(library_handler, "get_config");
     auto cfg = get_cfg();
+
     std::vector<int> real_inputs(4);
     std::iota(real_inputs.begin(), real_inputs.end(), 0);
-
     std::vector<std::unique_ptr<ValueType>> inputs;
     std::transform(real_inputs.begin(), real_inputs.end(), std::back_inserter(inputs), [](const auto &x) {
         return std::make_unique<IntValueType>(x);
@@ -26,6 +28,7 @@ int main() {
                    std::bind(&MapBase::map, cfg->getMapClass(), std::placeholders::_1));
 
     std::cout << dynamic_cast<IntValueType *>(cfg->getReduceClass()->reduce(outputs).get())->value;
+    dlclose(library_handler);
 
     return 0;
 }
