86a08434d9046a7a62dd758aef8c726d9b6b30a7
Roman Milishchuk <milromchuk@gmail.com>
2020-03-23 01:42:56 +0200

    Add serialization requirement to KeyValueType

--- types/KeyValueType.h
+++ types/KeyValueType.h
@@ -2,16 +2,30 @@
 #define MAP_REDUCE_KEYVALUETYPE_H
 
 #include <string>
+#include <sstream>
 
 class KeyValueType {
 public:
-    virtual void is_implemented() = delete;
+    virtual void parse(const std::string &) = 0;
+
+    virtual std::string to_string() = 0;
 };
 
+//TODO: add SFINAE to instantiate only with pod types
 template<typename T>
 class PrimitiveKeyValueType : public KeyValueType {
 public:
+    PrimitiveKeyValueType() = default;
+
     explicit PrimitiveKeyValueType(T i) : value(i) {}
+
+    void parse(const std::string &str) override {
+        std::stringstream(str) >> value;
+    }
+
+    std::string to_string() override {
+        return std::to_string(value);
+    }
 
     T value;
 };
