eaa68531a50879fda9d5844e510d4b268c1c9c4b
Roman Milishchuk <milromchuk@gmail.com>
2020-04-01 21:00:08 +0300

    Added PVS banner to all files

--- configurator/config.cpp
+++ configurator/config.cpp
@@ -1,3 +1,6 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 #include "config.h"
 
 std::shared_ptr<void> get_config_dll_handler(const std::filesystem::path &path) {
