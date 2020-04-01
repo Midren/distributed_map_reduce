18300e3f8565ded5c1f9534da7fe38fc574b3365
Roman Milishchuk <milromchuk@gmail.com>
2020-04-01 22:46:24 +0300

    Make run_example script user-independent

--- run_example.sh
+++ run_example.sh
@@ -1,4 +1,15 @@
-#cd cmake-build-debug && cmake .. && make -j4 && make install && cd -
+cd ..
 docker kill $(docker ps -q)
-for i in 1 2 3 4 5; do docker run -v /home/midren/ucu/distributed_db/distributed_map_reduce/:/home/mapreduce/distributed_map_reduce -itd milishchuk/mapreduce:latest /bin/bash; done
-echo "cd distributed_map_reduce  && rm -rf build && mkdir build && cd build && cmake .. && make -j4 && make install && cd ../example/ && rm -rf build && mkdir build && cd build &&cmake .. && make -j4 && export LD_LIBRARY_PATH=/home/mapreduce/distributed_map_reduce/example/build && ./example" | docker run -v /home/midren/ucu/distributed_db/distributed_map_reduce/:/home/mapreduce/distributed_map_reduce -a stdin -a stdout -a stderr -i milishchuk/mapreduce:latest
+for i in 1 2 3 4 5
+do
+    docker run -v $(pwd)/distributed_map_reduce/:/home/mapreduce/distributed_map_reduce -itd milishchuk/mapreduce:latest /bin/bash
+done
+echo "cd distributed_map_reduce && \
+      rm -rf build; mkdir build && cd build && \
+      cmake .. && make -j4 && make install && \
+      cd ../example/ && \
+      rm -rf build; mkdir build && cd build && \
+      cmake .. && make -j4 && \
+      export LD_LIBRARY_PATH=/home/mapreduce/distributed_map_reduce/example/build && \
+      ./example" | docker run -v $(pwd)/distributed_map_reduce/:/home/mapreduce/distributed_map_reduce -a stdin -a stdout -a stderr -i milishchuk/mapreduce:latest
+cd distributed_map_reduce
