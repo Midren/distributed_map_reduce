b7762ef835488502f57957bb9890e6c8b3989ed2
Roman Milishchuk <milromchuk@gmail.com>
2020-04-01 23:40:18 +0300

    More example code more expressive

--- example/config.cpp
+++ example/config.cpp
@@ -13,9 +13,12 @@ class square_map : public map_reduce::map_base {
 public:
     std::pair<std::unique_ptr<KeyValueType>, std::unique_ptr<KeyValueType>>
     map(const std::unique_ptr<KeyValueType> &key, const std::unique_ptr<KeyValueType> &value) override {
-        const auto input_value = dynamic_cast<IntKeyValueType *>(value.get());
-        return {std::make_unique<IntKeyValueType>(dynamic_cast<IntKeyValueType *>(key.get())->value),
-                std::make_unique<IntKeyValueType>(input_value->value * input_value->value)};
+
+        const auto key_in = dynamic_cast<IntKeyValueType &>(*key).value;
+        const auto value_in = dynamic_cast<IntKeyValueType &>(*value).value;
+
+        return {std::make_unique<IntKeyValueType>(key_in),
+                std::make_unique<IntKeyValueType>(value_in * value_in)};
     };
 };
 
@@ -24,22 +27,26 @@ public:
     std::pair<std::unique_ptr<KeyValueType>, std::unique_ptr<KeyValueType>>
     reduce(const std::unique_ptr<KeyValueType> &key,
            const std::vector<std::unique_ptr<KeyValueType>> &outputs) override {
-        return {std::make_unique<IntKeyValueType>(dynamic_cast<IntKeyValueType *>(key.get())->value),
-                std::make_unique<IntKeyValueType>(
-                        std::accumulate(outputs.begin(), outputs.end(), 0, [](int lhs, const auto &rhs) {
-                            return lhs + dynamic_cast<IntKeyValueType *>(rhs.get())->value;
-                        }))};
+
+        const auto key_out = dynamic_cast<IntKeyValueType &>(*key).value;
+        const auto value_res = std::accumulate(outputs.begin(), outputs.end(), 0, [](int lhs, const auto &rhs) {
+            const auto value_out = dynamic_cast<IntKeyValueType &>(*rhs).value;
+            return lhs + value_out;
+        });
+
+        return {std::make_unique<IntKeyValueType>(key_out),
+                std::make_unique<IntKeyValueType>(value_res)};
     }
 };
 
 std::shared_ptr<job_config> get_config() {
     return std::make_shared<job_config>("sum of squares",
-                                       std::move(std::make_unique<square_map>()),
-                                       std::move(std::make_unique<sum_reduce>()),
-                                       std::move(std::make_unique<IntKeyValueTypeFactory>()),
-                                       std::move(std::make_unique<IntKeyValueTypeFactory>()),
-                                       std::move(std::make_unique<IntKeyValueTypeFactory>()),
-                                       std::move(std::make_unique<IntKeyValueTypeFactory>()),
-                                       std::move(std::make_unique<IntKeyValueTypeFactory>())
+                                        std::move(std::make_unique<square_map>()),
+                                        std::move(std::make_unique<sum_reduce>()),
+                                        std::move(std::make_unique<IntKeyValueTypeFactory>()),
+                                        std::move(std::make_unique<IntKeyValueTypeFactory>()),
+                                        std::move(std::make_unique<IntKeyValueTypeFactory>()),
+                                        std::move(std::make_unique<IntKeyValueTypeFactory>()),
+                                        std::move(std::make_unique<IntKeyValueTypeFactory>())
     );
 }
